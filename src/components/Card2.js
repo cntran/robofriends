cd9694b57142db624b344ce778ebb977f37c059d
Craig Tran <craigtran@craigs-imac.lan>
2021-06-20 15:46:20 -0700

    pretteir

--- src/components/Card2.js
+++ src/components/Card2.js
@@ -1,18 +1,16 @@
-import React from 'react';
+import React from "react";
 
 const Card2 = (props) => {
-	const { email, name, id } = props;
-	return (
-		<div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'
-    
-    >
-<img alt='robots' src={`https://robohash.org/${id}?200x200`} />
-			<div>
-				<h2>{name}</h2>
-<p>{email}</p>
-			   </div>
-		</div>
-	);
-}
- 
+  const { email, name, id } = props;
+  return (
+    <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+      <img alt="robots" src={`https://robohash.org/${id}?200x200`} />
+      <div>
+        <h2>{name}</h2>
+        <p>{email}</p>
+      </div>
+    </div>
+  );
+};
+
 export default Card;
